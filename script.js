47c302fad5ed093b5c2f5600f08534da6103d289
GAMM95 <jhonatanmm.1995@gmail.com>
2023-04-29 19:24:18 -0500

    proyecto casi terminado

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Diccionario para encriptar y desencriptar
+// Diccionario de vocales a encriptar
 const mensajeEncriptado = text => {
   return text
-  .replace(/e/g, "enter")
-  .replace(/i/g, "imes")
-  .replace(/a/g, "ai")
-  .replace(/o/g, "ober")
-  .replace(/u/g, "ufat")
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat")
 }
-
+// Diccionario de vocales a desencriptar
 const mensajeDesencriptado = text => {
   return text
-  .replace(/enter/g, "e")
-  .replace(/imes/g, "i")
-  .replace(/ai/g, "a")
-  .replace(/ober/g, "o")
-  .replace(/ufat/g, "u")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u")
 }
 
-// Expresion regular para validar el texto
+// Restriccion de patrones para validar mensaje de entrada
 const pattern = /[A-ZÁÉÍÓÚÜÑáéíóúüñ0123456789]/;
 
 // Funcion para encriptar texto
@@ -28,19 +28,19 @@ function encriptar(e) {
   let text = document.getElementById('mensaje').value;
   // Validar si el texto cumple con la expresion regular
   if (pattern.test(text) == true) {
-      alterta();
-      sinResultado();
-      return;
+    alterta();
+    sinResultado();
+    return;
   }
   // Validar si hay texto para encriptar
   if (text != '') {
-      text = mensajeEncriptado(text);
-      resultado(text);
+    text = mensajeEncriptado(text);
+    resultado(text);
   } else {
-      sinResultado();
+    sinResultado();
   }
 }
-
+// Funcion para desencriptar texto
 function desencriptar(e) {
   e.preventDefault();
 
@@ -48,16 +48,16 @@ function desencriptar(e) {
   let text = document.getElementById('mensaje').value;
   // Validar si el texto cumple con la expresion regular
   if (pattern.test(text) == true) {
-      alterta();
-      sinResultado();
-      return;
+    alterta();
+    sinResultado();
+    return;
   }
   // Validar si hay texto para encriptar
   if (text != '') {
-      text = mensajeDesencriptado(text);
-      resultado(text);
+    text = mensajeDesencriptado(text);
+    resultado(text);
   } else {
-      sinResultado();
+    sinResultado();
   }
 }
 
@@ -65,30 +65,30 @@ function desencriptar(e) {
 async function copiarPortapapeles() {
   let text = document.getElementById('texto').innerHTML;
   try {
-      await navigator.clipboard.writeText(text);
-      // mostrar mensaje de copiado
-      console.log("Texto copiado al portapapeles");
+    await navigator.clipboard.writeText(text);
+    // mostrar mensaje de copiado
+    console.log("Texto copiado al portapapeles");
   } catch (err) {
-      // mostrar mensaje de no copiado
-      console.error("Error al copiar texto al portapapeles:", err);
+    // mostrar mensaje de no copiado
+    console.error("Error al copiar texto al portapapeles:", err);
   }
 }
 
 function resultado(text) {
   document.getElementById('texto').innerHTML = text;
-  document.getElementById('mensaje_inicial').classList.add('disable');
+  document.getElementById('mensaje_inicial').classList.add('disable'); // Ocultar imagen
   document.getElementById('resultado').classList.remove('disable');
 }
 
 function sinResultado() {
-  document.getElementById('mensaje_inicial').classList.remove('disable');
+  document.getElementById('mensaje_inicial').classList.remove('disable'); // seguir mostrando imagen
   document.getElementById('resultado').classList.add('disable');
 }
 
 /* Funcion para esconder mensaje de alerta cuando es necesario */
 function alterta() {
   document.getElementById('alertText').classList.remove('disable');
-  setTimeout(function() {
-      document.getElementById('alertText').classList.add('disable');
+  setTimeout(function () {
+    document.getElementById('alertText').classList.add('disable');
   }, 3000);
 }
